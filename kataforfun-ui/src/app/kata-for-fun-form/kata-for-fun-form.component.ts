bd81007c4ee7b1bc1295679329f79e20ee5d72e8
HDAG CMC <horus.dagnon@capco.com>
2023-06-07 15:31:52 +0200

    KataForFur_gui: adding form gui

--- kataforfun-ui/src/app/kata-for-fun-form/kata-for-fun-form.component.ts
+++ kataforfun-ui/src/app/kata-for-fun-form/kata-for-fun-form.component.ts
@@ -3,14 +3,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-kata-for-fun-form',
-  templateUrl: './kata-for-fun-form.component.html'
+  templateUrl: './kata-for-fun-form.component.html',
+  styleUrls: ['./kata-for-fun-form.component.css']
 })
 export class KataForFunFormComponent implements OnInit {
 
 
-  constructor() {
+  convertForm = this.formBuilder.group({
+    formInputNumber: ['', Validators.required]
+  })
 
-  }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
   }
